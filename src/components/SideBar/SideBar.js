545afdbf780854769d09ad30ff62ba62d75de22a
Bogdan <bveretelnik@gmail.com>
2021-02-22 19:50:25 +0200

    change and add components

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,8 +1,18 @@
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import React from "react";
 import "./SideBar.css";
 import AddIcon from "@material-ui/icons/Add";
-
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import SideBarOption from "./SideBarOption/SideBarOption";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LadelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 function SideBar() {
   return (
     <div className="sidebar">
@@ -12,6 +22,31 @@ function SideBar() {
       >
         Compose
       </Button>
+      <SideBarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SideBarOption Icon={StarIcon} title="Starred" number={54} />
+      <SideBarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SideBarOption Icon={LadelImportantIcon} title="Important" number={54} />
+      <SideBarOption Icon={NearMeIcon} title="Sent" number={54} />
+      <SideBarOption Icon={NoteIcon} title="Drafts" number={54} />
+      <SideBarOption Icon={ExpandMoreIcon} title="More" number={54} />
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
