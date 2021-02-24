5ed20716695ab557b850be0f50a36af97702a75f
Bogdan <bveretelnik@gmail.com>
2021-02-24 18:50:07 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import SideBar from "./components/SideBar/SideBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Mail from "./components/Mail/Mail";
+import EmailList from "./components/EmailList/EmailList";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <SideBar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <SideBar />
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
